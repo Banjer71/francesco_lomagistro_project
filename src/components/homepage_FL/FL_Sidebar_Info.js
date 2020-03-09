c2adc7dc6d48087f0de2020ed9f5bbdba24aac4d
Banjer71 <davgio7@gmail.com>
2020-03-09 20:37:08 +0000

    add more pictures and text

--- src/components/homepage_FL/FL_Sidebar_Info.js
+++ src/components/homepage_FL/FL_Sidebar_Info.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import fl1 from '../../images/francesco_top.jpg';
 import fl2 from '../../images/main_large.jpg';
+import fl3 from '../../images/fl3.jpg'
+import fl4 from '../../images/fl4.jpg'
 
 const FL_Sidebar_Info = () => {
 	const imageRef = useRef(null);
@@ -41,6 +43,57 @@ const FL_Sidebar_Info = () => {
 				Francesco Lomagsitro
 			</h1>
 			<p className="info-text">
+				Batterista/Percussionista/Insegnante da diversi anni sulle scene musicali nazionali ed internazionali,
+				il suo approccio alla musica inizia con lo studio del pianoforte all’età di 11 anni per poi passare alla
+				batteria a 16. Inizia da autodidatta per poi prendere le prime lezioni e in seguito approfond la sua
+				formazione partecipando a workshop e seminari con i "Big" dello strumento. Ha conseguito la Laurea in
+				“Strumenti a Percussioni” presso il Conservatorio N. Piccinni di Bari , ed in seguito la specialistica
+				di II livello per la Didattica per lo Strumento Musicale.
+			</p>
+			<p className="info-text">
+				La sua versatilità gli ha permesso di avere una intensa attività concertistica, diversificando la stessa
+				in più ambiti e generi: dalla classica al pop , dal jazz allo studio recording, condividendo il palco
+				con diversi artisti internazionali esibendosi in diversi paesi e continenti come Europa, Stati Uniti e
+				Asia; ecco alcuni di loro: Kay F. Jackson, Joice Youlle, Kelly Joyce, Sandra St. Victor, Wendy Lewis,
+				Madame Pat Tandy, Mariella Nava, Antonella Ruggero, Milva, Cecilia Gasdia, Elio, Erz, Freak Antoni, Enzo
+				Gragnaniello, Tony Esposito, Eugenio Bennato, Mimmo Locasciulli, Mario Rosini, Fabrizio Sotti, Chihiro
+				Yamanaka, Rick Stone, Rotem Sivam, Marcus Machado, David Schnitter , Vito Di Modugno, Fabio Morgera,
+				Carol Sudhalter, Cicci Santucci, Marco Tamburini, Dario Deidda, Max Ionata, Gigi Cifarelli, Roberto
+				Ottaviano, Gaetano Partipilo, Fabio Giachino, Barend Middlehoff, Teo Ciavarella ,Orchestra del DAMS di
+				Bologna e tanti altri musisti di spicco della sua area geografica e musicale.
+			</p>
+				<img src={fl1} alt="T-riot on stage" />
+			<p className="info-text">
+				Oltre alle innumerevoli collaborazioni da sideman , nel 2005 da vita a dei progetti che lo vedono in
+				prima linea come leader ; primo fra tutti e in collaborazione con Ettore Carucci, i Berardi Jazz
+				Connection, un progetto jazz annoverato fra i più di spicco nel panorama del contemporany jazz europeo
+				con i quali ha rilasciato 4 cd conosciuti in tutto il mondo, Giappone ,Usa e Nord Europa in testa;
+				alcune composizioni sono state incluse in compilation di label e network mondiali. Nel 2016 ha formato
+				un nuovo progetto, i T-Riot, con i quali ha prodotto “A Different Truth” cd edito per l’etichetta
+				giapponese Albòre Jazz, conquistando nuovi estimatori in Giappone , Nord Europa, Sud Africa e Stati
+				Uniti; nominato fra i migliori dischi del 2017 per Radio Jazz fm di Londra, e ai primi posti della Top
+				Jazz Charts di iTunes in Finlandia e Vietnam, nella primavera del 2018 faranno un tour esibendosi in
+				teatri e jazz club in Thailandia e Cina
+			</p>
+			<img src={fl2} alt="T-riot on stage" />
+			<p className="info-text">
+				Nel corso della sua carriera ha registrato per etichette discografiche come Virgin, Universal, Hobo,
+				Sony, Edel, Storie di Note, Irma Records, Family Affair, Dodicilune, Antibemusic, Flaminio Jazz, Caffè
+				Concerto, Four, Albòre Jazz. E' stato intervistato e recensito dalle più importanti riviste di settore
+				nazionali e internazionali. E' endorser internazionale dei marchi Paiste e Vic Firth.
+			</p>
+			
+			<h1 className="pro" id="triot" ref={imageRef}>
+				Info Lesson
+			</h1>
+			<p className='info-text'>
+				Per prenotazioni e lezioni cliccare in fondo alla pagina nella sezione contatti
+			</p>
+			<img src={fl4} alt="T-riot on stage" />
+			<hr/>
+			<h1>English section</h1>
+			
+			<p className="info-text">
 				Francesco Lomagistro, began piano studies at age of 11 and later switched to drums. He received his
 				music education through private lessons and attendance at seminars by great musical Masters. He received
 				his Degree in ‘Percussion Instruments’ at the “Niccolo Piccinni” Music Conservatory in Bari, Italy and
@@ -48,18 +101,11 @@ const FL_Sidebar_Info = () => {
 				in Italy and make clinics over all the Country.
 			</p>
 			<p className="info-text">
-				He leads the group Berardi Jazz Connection, a European jazz project whose release 4 cd well known around
-				the world, Japan ahead, and Usa and North Europe, and since 2016 he formed a new trio project the
-				T-Riot, with which he released the first cd "A different truth" for the japanese label Albòre Jazz,
-				conquering new fans in Japan, North Europe, South Africa and Usa; the cd has been in some jazz top
-				charts, and on May 2018 they made a tour in Asia , playing in theatres and venues in Bangkok and Honk
-				Kong.
-			</p>
-			<p className="info-text">
 				His versatility has afforded him experience in different musical ambients collaborating with a lot of
 				different artists; some of his compositions have been included in many compilations around the world and
 				have been featured in the top jazz charts.
 			</p>
+			<img src={fl3} alt="T-riot on stage" />
 			<p className="info-text">
 				He has participated in different recording sessions for different labels such as Virgin, Universal,
 				Hobo-Sony, Edel, Storie di Note, Irma Records, Antibemusic, Family Affairs, Flaminio Jazz, P-Vine
@@ -69,31 +115,19 @@ const FL_Sidebar_Info = () => {
 				He performed in different venues as theatres, clubs and festivals in Europe, USA and Asia. He is
 				international endorser with Paiste and Vic Firth.
 			</p>
-			<h1 className="pro" id="triot" ref={imageRef}>
-				Info Lesson
-			</h1>
-			<img src={fl1} alt="T-riot on stage" />
-			<p className="info-text">
-				A rock-solid trio session, with a top-shelf Italian groove pedigree as the group features two members
-				from Berardi Jazz Connection, and the pianist from Quintetto X! You might remember those two groups from
-				their more club jazz-oriented projects from the previous decade or two,but this time around, all three
-				musicians are in a straighter jazz vein, yet still one that's filled with plenty of rhythm! The piano,
-				bass and drums come together fantastically here with Pietro Vincenti on acoustic keys, Camillo Pace on
-				bass, and Francesco Lomagistro on drums.
-			</p>
 			<h1 className="pro" id="bjc" ref={imageRef}>
-				Educator
+				Lead Member
 			</h1>
-			<img src={fl2} alt="Berardi Jazz Connection band" />
+			
 			<p className="info-text">
-				Berardi Jazz Connection was birthed by Francesco Lomagistro’s well-known drummer/percussionist idea to
-				put together all the experiences that come from his passion for Afro – american music, the ‘groove’, and
-				the synergy between rhythm and melody. The voyage started many years ago in the center of Taranto (south
-				of Italy) on a street called Via Berardi where, Francesco had an apartment using it as a rehearsal
-				studio. After many different experiences as “sideman” and different concerts around the world with both
-				artists, in the year 2005 Francesco decided to “give life” to this project involving other Italian
-				talents and international sp guests.
+				He leads the group Berardi Jazz Connection, a European jazz project whose release 4 cd well known around
+				the world, Japan ahead, and Usa and North Europe, and since 2016 he formed a new trio project the
+				T-Riot, with which he released the first cd "A different truth" for the japanese label Albòre Jazz,
+				conquering new fans in Japan, North Europe, South Africa and Usa; the cd has been in some jazz top
+				charts, and on May 2018 they made a tour in Asia , playing in theatres and venues in Bangkok and Honk
+				Kong.
 			</p>
+			<p className="info-text" />
 		</div>
 	);
 };
--- src/components/projects/Projects_Sidebar.js
+++ src/components/projects/Projects_Sidebar.js
@@ -19,7 +19,6 @@ const Projects_Sidebar = () => {
 
 		if (window.innerWidth > 960 && image) {
 			if (rect.top - window.innerHeight === 0 && rect.top - window.innerHeight > -259) {
-				console.log(rect.top - window.innerHeight);
 				bodyClass = 'section4';
 				flip.innerText = 'My Projects';
 				flip.style.color = '#f9c943';
@@ -36,6 +35,32 @@ const Projects_Sidebar = () => {
 		document.body.className = bodyClass;
 	};
 
+	function getDocHeight() {
+		var D = document;
+		return Math.max(
+			D.body.scrollHeight, D.documentElement.scrollHeight,
+			D.body.offsetHeight, D.documentElement.offsetHeight,
+			D.body.clientHeight, D.documentElement.clientHeight
+		)
+	}
+	 
+	const docheight = getDocHeight()
+
+	function amountscrolled(){
+		var winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+		var docheight = getDocHeight()
+		var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+		var trackLength = docheight - winheight
+		var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+		console.log(pctScrolled + '% scrolled')
+	}
+
+	
+	 
+	window.addEventListener("scroll", function(){
+		amountscrolled()
+	}, false)
+
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
 		return () => {
@@ -46,36 +71,34 @@ const Projects_Sidebar = () => {
 	return (
 		<div className="info">
 			<h1 className="pro" id="triot" ref={imageRef}>
-				Projects
-			</h1>
-			<p className="info-text">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, illo quasi saepe inventore iusto
-				facere maxime dolorum provident fugit assumenda. Adipisci aut sequi vel ex qui obcaecati repellat ipsum
-				aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, illo quasi saepe inventore
-				iusto facere maxime dolorum provident fugit assumenda. Adipisci aut sequi vel ex qui obcaecati repellat
-				ipsum aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, illo quasi saepe
-				inventore iusto facere maxime dolorum provident fugit assumenda. Adipisci aut sequi vel ex qui obcaecati
-				repellat ipsum aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-			</p>
-
-			<h1 className="pro" id="triot" ref={imageRef}>
 				T-RIOT
 			</h1>
-			<p className="info-text">
-				A rock-solid trio session, with a top-shelf Italian groove pedigree as the group features two members
-				from Berardi Jazz Connection, and the pianist from Quintetto X! You might remember those two groups from
-				their more club jazz-oriented projects from the previous decade or two,but this time around, all three
-				musicians are in a straighter jazz vein, yet still one that's filled with plenty of rhythm! The piano,
-				bass and drums come together fantastically here with Pietro Vincenti on acoustic keys, Camillo Pace on
-				bass, and Francesco Lomagistro on drums.
+			<p className="info-text" style={{ fontStyle: 'italic', fontSize: '15px', padding: '30px' }}>
+				A rock-solid trio session, with a top-shelf Italian groove pedigree ...
 			</p>
 			<p className="info-text">
-				A rock-solid trio session, with a top-shelf Italian groove pedigree as the group features two members
-				from Berardi Jazz Connection, and the pianist from Quintetto X! You might remember those two groups from
-				their more club jazz-oriented projects from the previous decade or two,but this time around, all three
-				musicians are in a straighter jazz vein, yet still one that's filled with plenty of rhythm! The piano,
-				bass and drums come together fantastically here with Pietro Vincenti on acoustic keys, Camillo Pace on
-				bass, and Francesco Lomagistro on drums.
+				Questa la descrizione di “Dusty Groove” (Chicago ) di questo nuovo progetto tutto pugliese ma ormai di
+				caratura internazionale.
+			</p>
+			<p className="info-text">
+				Tre musicisti pugliesi da tempo noti alle scene jazz e non solo, nazionali ed internazionali, che si
+				rincontrano dopo diversi anni di perigrazioni sonore personali e variegate. La band è stata formata alla
+				fine del 2015, grazie al batterista Francesco Lomagistro. Pietro Vincenti al piano, Camillo Pace al
+				contrabbasso e Francesco Lomagistro alla batteria, convogliano le loro esperienze e la loro affinità
+				artistica in un nuovo viaggio che si chiamerà "A Different Truth", il primo album di questa band,
+				interamente registrato dal vivo nel Maggio 2016 e prodotto e distribuito un anno dopo dall’etichetta
+				giapponese Albòre Jazz di Satoshi Toyoda. L’album sarà nominato fra i migliori dell’anno da Jazz Fm
+				(Londra) e sarà in vetta alle classifiche di iTunes Jazz in vari paesi di Europa, Finlandia in testa;
+				verrà programmato da diversi network radiofonici nel mondo e otterrà diversi consensi dalla stampa
+				internazionale. Il trio conquisterà il pubblico asiatico con un tour conclusosi a Giugno 2018 toccando
+				città come Bangkok e Hong Kong
+			</p>
+			<p className="info-text" style={{ fontStyle: 'italic', fontSize: '15px' }}>
+				“Energia frizzante messa su con criterio e con l’eleganza tipica degli anni Sessanta-Settanta, senza
+				alcuna copiatura.” - Musica Jazz
+			</p>
+			<p className="info-text" style={{ fontStyle: 'italic', fontSize: '15px' }}>
+				"It’s a super album." - Ruth Fisher (Presenter Jazz FM London)
 			</p>
 			<img src={Triot} alt="T-riot on stage" />
 			<div className="band-members">
@@ -106,23 +129,40 @@ const Projects_Sidebar = () => {
 				Berardi Jazz Connection
 			</h1>
 			<p className="info-text">
-				Berardi Jazz Connection was birthed by Francesco Lomagistro’s well-known drummer/percussionist idea to
-				put together all the experiences that come from his passion for Afro – american music, the ‘groove’, and
-				the synergy between rhythm and melody. The voyage started many years ago in the center of Taranto (south
-				of Italy) on a street called Via Berardi where, Francesco had an apartment using it as a rehearsal
-				studio. After many different experiences as “sideman” and different concerts around the world with both
-				artists, in the year 2005 Francesco decided to “give life” to this project involving other Italian
-				talents and international sp guests.
+				I BJC nascono dall' idea di Francesco Lomagistro (Batteria e Percussioni) di convogliare tutte le
+				esperienze nella passione per la musica Afroamericana, la ricerca del “groove", la sinergia fra ritmo e
+				melodia.. Ettore Carucci (Piano) è co-Leader e compagno indispensabile, nonchè al timone con Francesco
+				in questo viaggio iniziato diversi anni fa, la cui partenza è proprio da via "Berardi", una via del
+				centro di Taranto, dove Francesco aveva la sua reharsal room . Dopo diverse esperienze come "sidemen"
+				nel pop, nel jazz, e altri contesti, Francesco ed Ettore decidono di dare vita a questo progetto. i
+				Primi 3 dischi sono stati prodotti da Antibemusic (Italy) e P-Vine Records (Giappone).
 			</p>
 			<img src={bjc} alt="Berardi Jazz Connection band" />
 			<p className="info-text">
-				Berardi Jazz Connection was birthed by Francesco Lomagistro’s well-known drummer/percussionist idea to
-				put together all the experiences that come from his passion for Afro – american music, the ‘groove’, and
-				the synergy between rhythm and melody. The voyage started many years ago in the center of Taranto (south
-				of Italy) on a street called Via Berardi where, Francesco had an apartment using it as a rehearsal
-				studio. After many different experiences as “sideman” and different concerts around the world with both
-				artists, in the year 2005 Francesco decided to “give life” to this project involving other Italian
-				talents and international sp guests.
+				Con i loro 3 dischi The way i like , Do it e Anyway si sono conquistati un posto nel panorama fra le
+				nuove realtà del jazz europeo, sono stati programmati da diverse radio di Europa, Giappone e Stati
+				Uniti.. Conosciuti dal grande pubblico anche grazie a “Change” brano della nota band anni 80, i Tears
+				for Fears, arrangiato in occasione dell’uscita del secondo album “Do it” e programmato all’estero e in
+				Italia da diversi network come Radio Montecarlo, Capital, Radio Dj. Una loro composizione Walking in the
+				Village tratta dall’ultimo album Anyway, è stata prescelta fra i 25 top tunes del 2010 in Giappone da
+				Tokyo Jazz Notes. il 4° disco “A new journey “ vede al timone solo Francesco Lomagistro che sarà
+				accompagnato da alcuni nuovi elementi nella band , come Michele Campobasso al e Francesco Lento alla
+				tromba.
+			</p>
+			<p className="info-text" style={{ fontStyle: 'italic', fontSize: '14px' }}>
+				..A cool combo from the Italian scene --The sound here is a great mix of modern club jazz and older 60s
+				modes -- played in styles that recall the sound of Prestige or Riverside from years back, but which also
+				tunes the rhythms towards some of the more contemporary modes explored by the best underground jazz
+				groups of the European scene of late!<br />
+				<span style={{ fontWeight: 'Bold' }}>“Dusty Groove“</span>
+			</p>
+			<p className="info-text" style={{ fontStyle: 'italic', fontSize: '14px' }}>
+				This Italian band meshes tasteful, modern production with incredible live' playing, and the result is a
+				joyous celebration of jazz artistry. The grooves are a hipster's paradise, yet the overall sound is
+				accessible to everyone. It's one of the top CDs in my stack by a band that I'd love to see in a smoky
+				club, with a neat martini in one hand and a sloppy cigar in the other. Picks: Mr. Rhodes, Offside, Una
+				Mas.<br />
+				<span style={{ fontWeight: 'bold' }}>“RAFE GOMEZ (Dj network Smoth Jazz 101.9 New York)“</span>
 			</p>
 			<div className="band-members">
 				<ul className="triot-band-members">
@@ -147,6 +187,31 @@ const Projects_Sidebar = () => {
 				<SocialMedia icon={facebook_logo} link="https://www.facebook.com/berardijazzconnection/" />
 				<SocialMedia icon={spotify_logo} link="https://open.spotify.com/artist/6GUaa8teYHKlgnUeUAWQHc" />
 			</div>
+			<hr style={{margin: '2rem'}}/>
+			<h1>English Section</h1>
+			<h1 className="pro" id="triot" ref={imageRef}>
+				T-RIOT
+			</h1>
+			<p className="info-text">
+				A rock-solid trio session, with a top-shelf Italian groove pedigree as the group features two members
+				from Berardi Jazz Connection, and the pianist from Quintetto X! You might remember those two groups from
+				their more club jazz-oriented projects from the previous decade or two,but this time around, all three
+				musicians are in a straighter jazz vein, yet still one that's filled with plenty of rhythm! The piano,
+				bass and drums come together fantastically here with Pietro Vincenti on acoustic keys, Camillo Pace on
+				bass, and Francesco Lomagistro on drums.
+			</p>
+			<h1 className="pro" id="bjc" ref={imageRef}>
+				Berardi Jazz Connection
+			</h1>
+			<p className="info-text">
+				Berardi Jazz Connection was birthed by Francesco Lomagistro’s well-known drummer/percussionist idea to
+				put together all the experiences that come from his passion for Afro – american music, the ‘groove’, and
+				the synergy between rhythm and melody. The voyage started many years ago in the center of Taranto (south
+				of Italy) on a street called Via Berardi where, Francesco had an apartment using it as a rehearsal
+				studio. After many different experiences as “sideman” and different concerts around the world with both
+				artists, in the year 2005 Francesco decided to “give life” to this project involving other Italian
+				talents and international sp guests.
+			</p>
 		</div>
 	);
 };
