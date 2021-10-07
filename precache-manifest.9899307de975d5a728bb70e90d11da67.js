87609b9bb3153133e84605bbd43a68de76f44a45
Banjer71 <davgio7@gmail.com>
2021-10-07 18:12:47 +0100

    Updates

---
+++ precache-manifest.9899307de975d5a728bb70e90d11da67.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2f90dae4283c7367d7981822e70468",
+    "url": "/francesco_lomagistro_project/index.html"
+  },
+  {
+    "revision": "831d650621b183536f7f",
+    "url": "/francesco_lomagistro_project/static/css/main.b58dda3d.chunk.css"
+  },
+  {
+    "revision": "a6bb87e9dc21cf6ee116",
+    "url": "/francesco_lomagistro_project/static/js/2.fbe61d1c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/francesco_lomagistro_project/static/js/2.fbe61d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831d650621b183536f7f",
+    "url": "/francesco_lomagistro_project/static/js/main.f0138de0.chunk.js"
+  },
+  {
+    "revision": "9fa81891be0170a3daaa",
+    "url": "/francesco_lomagistro_project/static/js/runtime-main.ecf47480.js"
+  },
+  {
+    "revision": "d475a73f3ecd6e6c1232ae201e718395",
+    "url": "/francesco_lomagistro_project/static/media/bjc.d475a73f.jpg"
+  },
+  {
+    "revision": "4f9ea67f474834c36934fd8158621fe9",
+    "url": "/francesco_lomagistro_project/static/media/bjc_founders.4f9ea67f.jpg"
+  },
+  {
+    "revision": "b9173756331f75b544efeff99854be94",
+    "url": "/francesco_lomagistro_project/static/media/facebook.b9173756.svg"
+  },
+  {
+    "revision": "0992b63f1be248fd1afe82851c226527",
+    "url": "/francesco_lomagistro_project/static/media/fl3.0992b63f.jpg"
+  },
+  {
+    "revision": "ec347aa8fa8295c05a3fc6443b3e8075",
+    "url": "/francesco_lomagistro_project/static/media/fl4.ec347aa8.jpg"
+  },
+  {
+    "revision": "41f6ec64ed1324fbabd2c5c9892a5e79",
+    "url": "/francesco_lomagistro_project/static/media/francesco_top.41f6ec64.jpg"
+  },
+  {
+    "revision": "f73909b75607fa8d122fe05ad059a0f0",
+    "url": "/francesco_lomagistro_project/static/media/instagram.f73909b7.svg"
+  },
+  {
+    "revision": "e93d576182b97d2e874ee8137dbb4af7",
+    "url": "/francesco_lomagistro_project/static/media/mail.e93d5761.svg"
+  },
+  {
+    "revision": "7979375324636dba3b85bce585bb4e58",
+    "url": "/francesco_lomagistro_project/static/media/main_large.79793753.jpg"
+  },
+  {
+    "revision": "56eab5ba7f57dd3f5d87dce38b8f0f95",
+    "url": "/francesco_lomagistro_project/static/media/spotify.56eab5ba.svg"
+  },
+  {
+    "revision": "8545136bd87f5aa64dbc1f0f7c67d360",
+    "url": "/francesco_lomagistro_project/static/media/thumb.8545136b.jpg"
+  },
+  {
+    "revision": "b44642924b89d62b01e5221db736e227",
+    "url": "/francesco_lomagistro_project/static/media/thumb_paiste.b4464292.jpg"
+  },
+  {
+    "revision": "9c2106f9345972e55fd69435fc6f4495",
+    "url": "/francesco_lomagistro_project/static/media/thumb_triot.9c2106f9.jpg"
+  },
+  {
+    "revision": "0e7bb453baffc291ea6d2a3444d6f8c7",
+    "url": "/francesco_lomagistro_project/static/media/triot_stage.0e7bb453.jpg"
+  },
+  {
+    "revision": "e8054430caf1c5baa0358c3cb058d0a0",
+    "url": "/francesco_lomagistro_project/static/media/video_05.e8054430.jpg"
+  },
+  {
+    "revision": "88fbb9d2e3d1100412006d92476c41ee",
+    "url": "/francesco_lomagistro_project/static/media/video_3.88fbb9d2.jpg"
+  },
+  {
+    "revision": "7ec02f80913fadad5f99a2404732cfe8",
+    "url": "/francesco_lomagistro_project/static/media/youtube.7ec02f80.svg"
+  }
+]);
