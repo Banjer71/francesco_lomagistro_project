156ff8d5777749dd738fab7356fe3807fe8e67f1
Banjer71 <davgio7@gmail.com>
2021-10-07 18:28:13 +0100

    added new configurations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {  HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import Header from "./components/header/Header";
