09488aac9b75d9ab180a24932f8f582de24120f7
unknown <davgio7@gmail.com>
2025-09-15 16:52:32 +0100

    Updates

---
+++ precache-manifest.c9244ae731d60d9219acc0271c8a6429.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc2844dee8fa49399a5bbd1e4a968f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b140c3ea23329800b52",
+    "url": "/static/css/main.be33c72e.chunk.css"
+  },
+  {
+    "revision": "fb3671137d71a04acdbd",
+    "url": "/static/js/2.4554df22.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4554df22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b140c3ea23329800b52",
+    "url": "/static/js/main.bf91f6c3.chunk.js"
+  },
+  {
+    "revision": "df32612d945c8668340d",
+    "url": "/static/js/runtime-main.309d46d3.js"
+  },
+  {
+    "revision": "d475a73f3ecd6e6c1232ae201e718395",
+    "url": "/static/media/bjc.d475a73f.jpg"
+  },
+  {
+    "revision": "4f9ea67f474834c36934fd8158621fe9",
+    "url": "/static/media/bjc_founders.4f9ea67f.jpg"
+  },
+  {
+    "revision": "11b5a65ebdbe6433776e637d845f6f08",
+    "url": "/static/media/book_cover_p.11b5a65e.jpg"
+  },
+  {
+    "revision": "8bd88119acbfee24d3fe6d001ba96ee9",
+    "url": "/static/media/cover_book.8bd88119.jpg"
+  },
+  {
+    "revision": "b9173756331f75b544efeff99854be94",
+    "url": "/static/media/facebook.b9173756.svg"
+  },
+  {
+    "revision": "0992b63f1be248fd1afe82851c226527",
+    "url": "/static/media/fl3.0992b63f.jpg"
+  },
+  {
+    "revision": "ec347aa8fa8295c05a3fc6443b3e8075",
+    "url": "/static/media/fl4.ec347aa8.jpg"
+  },
+  {
+    "revision": "41f6ec64ed1324fbabd2c5c9892a5e79",
+    "url": "/static/media/francesco_top.41f6ec64.jpg"
+  },
+  {
+    "revision": "d653773a09558fe61536a0eb95419fb0",
+    "url": "/static/media/hero-desktop-fl.d653773a.jpg"
+  },
+  {
+    "revision": "f73909b75607fa8d122fe05ad059a0f0",
+    "url": "/static/media/instagram.f73909b7.svg"
+  },
+  {
+    "revision": "14f032dfe77b21656a666d00f154b066",
+    "url": "/static/media/libro_foto.14f032df.jpg"
+  },
+  {
+    "revision": "e93d576182b97d2e874ee8137dbb4af7",
+    "url": "/static/media/mail.e93d5761.svg"
+  },
+  {
+    "revision": "7979375324636dba3b85bce585bb4e58",
+    "url": "/static/media/main_large.79793753.jpg"
+  },
+  {
+    "revision": "80f020dd103dba27a1240617d4acac8d",
+    "url": "/static/media/monile-header.80f020dd.jpg"
+  },
+  {
+    "revision": "56eab5ba7f57dd3f5d87dce38b8f0f95",
+    "url": "/static/media/spotify.56eab5ba.svg"
+  },
+  {
+    "revision": "8545136bd87f5aa64dbc1f0f7c67d360",
+    "url": "/static/media/thumb.8545136b.jpg"
+  },
+  {
+    "revision": "b44642924b89d62b01e5221db736e227",
+    "url": "/static/media/thumb_paiste.b4464292.jpg"
+  },
+  {
+    "revision": "9c2106f9345972e55fd69435fc6f4495",
+    "url": "/static/media/thumb_triot.9c2106f9.jpg"
+  },
+  {
+    "revision": "a4caff469ad9abc54cd0bbce9ed08f89",
+    "url": "/static/media/triot.a4caff46.jpg"
+  },
+  {
+    "revision": "0e7bb453baffc291ea6d2a3444d6f8c7",
+    "url": "/static/media/triot_stage.0e7bb453.jpg"
+  },
+  {
+    "revision": "e8054430caf1c5baa0358c3cb058d0a0",
+    "url": "/static/media/video_05.e8054430.jpg"
+  },
+  {
+    "revision": "88fbb9d2e3d1100412006d92476c41ee",
+    "url": "/static/media/video_3.88fbb9d2.jpg"
+  },
+  {
+    "revision": "7ec02f80913fadad5f99a2404732cfe8",
+    "url": "/static/media/youtube.7ec02f80.svg"
+  }
+]);
